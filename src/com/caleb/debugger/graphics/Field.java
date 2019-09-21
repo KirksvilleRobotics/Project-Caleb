abf04bbeaeb2c4168694e36c537f61081a2e14f4
Ben <bvazzana672@kirksville.k12.mo.us>
2019-09-20 22:27:52 -0500

    added javadocs

--- src/com/caleb/debugger/graphics/Field.java
+++ src/com/caleb/debugger/graphics/Field.java
@@ -1,4 +1,55 @@
 package com.caleb.debugger.graphics;
 
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+/**
+ * Field
+ *
+ * This class represents the field for the robot challenge.
+ * It uses the java.awt.Canvas class to handle all the rendering, and
+ * it needs an image file of the same dimensions as the field to draw
+ *
+ */
 public class Field {
+
+    private Canvas canvas;
+    private BufferedImage fieldImg;
+
+    /**
+     * Constructor for the field class
+     *
+     * This constructor creates a canvas, and it initializes the
+     * BufferedImage to draw the field
+     * @param width field width in pixels
+     * @param height field height in pixels
+     */
+    public Field(int width, int height) {
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+    }
+
+    /**
+     * This function is to be called each frame inside of a window
+     * loop. It gets a BufferStrategy from the canvas, and uses the
+     * BufferStrategy's getDrawGraphics() method to draw the field
+     */
+    public void render() {
+        BufferStrategy bs = canvas.getBufferStrategy();
+        if(bs == null) {
+            canvas.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        //TODO: get image of field, initialize the BufferedImage, and render that instead
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        g.dispose();
+        bs.show();
+    }
+
 }
--- src/com/caleb/debugger/graphics/Window.java
+++ src/com/caleb/debugger/graphics/Window.java
@@ -3,21 +3,48 @@ package com.caleb.debugger.graphics;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Window
+ *
+ * This class represents the window for the application. The window
+ * for this project consists of two main sections: the field, and
+ * the info section
+ */
 public class Window {
     private JFrame frame;
     private JPanel contentPane;
 
+    /**
+     * Constructor for the window
+     *
+     * This constructor initializes the JFrame, creates the
+     * contentPane, and adds the application's components
+     * @param title String representing the title of the window
+     * @param width window width in pixels
+     * @param height window height in pixels
+     */
     public Window(String title, int width, int height) {
         frame = new JFrame(title);
 
         contentPane = (JPanel) frame.getContentPane();
         contentPane.setPreferredSize(new Dimension(width, height));
-        contentPane.setLayout(null);
+        contentPane.setLayout(new FlowLayout(FlowLayout.LEADING, 0, 0));
         frame.pack();
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
+    }
+
+    /**
+     * Render
+     *
+     * This method updates the graphical display of the window and
+     * of all its components. It is meant to be called within a
+     * loop each frame
+     */
+    public void render() {
+
     }
 }
