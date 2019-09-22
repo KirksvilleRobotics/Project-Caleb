a558f6a21dcf5c02732551dde99a4cc564f0c41b
chris <christopherscottmarshall@gmail.com>
2019-09-21 21:29:08 -0500

    Update JavaDocs

--- src/com/caleb/client/Debugger.java
+++ src/com/caleb/client/Debugger.java
@@ -3,11 +3,17 @@ package com.caleb.client;
 import java.io.IOException;
 import java.net.*;
 
-import static com.caleb.client.SerializeData.serialzeData;
+import static com.caleb.client.SerializeData.serializeData;
 
-//TODO JavaDoc
 /**
+ * Debugger
  *
+ * This class is the core of a client side FTC debugging utility.
+ * To use this class, you must set up a connection to the server
+ * side of the debugging software by defining an ip address and a
+ * port for the socket. After that, you can send packets of data
+ * the .send() function. Each packet contains the robot's x, y, and
+ * theta.
  */
 public class Debugger {
     private InetAddress ip;
@@ -15,9 +21,10 @@ public class Debugger {
     private int port;
 
     /**
+     * Sets up a connection to the debugger
      *
-     * @param address
-     * @param port
+     * @param address ip address of the debugging software
+     * @param port port for the socket to be created on
      * @throws UnknownHostException
      * @throws SocketException
      */
@@ -28,13 +35,17 @@ public class Debugger {
     }
 
     /**
+     * This function sends a UDP data packet to the debugging
+     * software. It uses the function serializeData() to create
+     * a byte array of robotX, robotY, and robotTheta. Packets are
+     * sent on their own thread.
      *
-     * @param robotX
-     * @param robotY
-     * @param robotTheta
+     * @param robotX robot's current x coordinate
+     * @param robotY robot's current y coordinate
+     * @param robotTheta robot's current theta
      */
     public void send(double robotX, double robotY, double robotTheta) {
-        byte[] data = serialzeData(robotX, robotY, robotTheta);
+        byte[] data = serializeData(robotX, robotY, robotTheta);
 
         Thread send = new Thread(() -> {
             DatagramPacket packet = new DatagramPacket(data, data.length, ip, port);
--- src/com/caleb/client/SerializeData.java
+++ src/com/caleb/client/SerializeData.java
@@ -2,21 +2,26 @@ package com.caleb.client;
 
 import java.nio.ByteBuffer;
 
-//TODO JavaDoc
 /**
+ * SerializeData
  *
+ * This is a static utils class that serializes a packet
+ * of data to send to the debugger software
  */
 public class SerializeData {
     private SerializeData() {}
 
     /**
+     * This function serializes data into a byte array. The first 8
+     * bytes correspond to the x coordinate, the next 8 correspond
+     * to the y coordinate, and the last 8 correspond to the theta.
      *
-     * @param robotX
-     * @param robotY
-     * @param robotTheta
-     * @return
+     * @param robotX robot's x coordinate
+     * @param robotY robot's y coordinate
+     * @param robotTheta robot's theta
+     * @return a byte array of size 24
      */
-    public static byte[] serialzeData(double robotX, double robotY, double robotTheta) {
+    public static byte[] serializeData(double robotX, double robotY, double robotTheta) {
         byte[] byteX = new byte[8];
         ByteBuffer.wrap(byteX).putDouble(robotX);
 
